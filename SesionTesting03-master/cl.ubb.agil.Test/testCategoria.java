92a521a69ca22b19826f3ad81dc0d19c5fb7c450
matias01 <mat16@live.cl>
2016-04-20 12:20:39 -0300

    Test unitario de Categoria   metodo getNombre.

--- SesionTesting03-master/cl.ubb.agil.Test/testCategoria.java
+++ SesionTesting03-master/cl.ubb.agil.Test/testCategoria.java
@@ -1,4 +1,41 @@
+import org.junit.Before;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+
+import cl.ubb.agil.Categoria;
+import cl.ubb.agil.Producto;
+
 
 public class testCategoria {
+
+private Categoria categotia;
+
+
+
+private String nombre = "guitarra electrica";
+private String codigo = "aa000";
+
+@Before 
+public void setup() throws Exception{
+ categotia= new Categoria(nombre, codigo);
+	 System.out.println("paso");
+}
+
+@Test
+public void getNombreGuitarraElectrica(){
+	 /*arrange*/
+	
+	 /*act*/
+	 String resultado=categotia.getNombre();
+	 
+	 /*assert*/
+	 assertThat(resultado,is("guitarra electrica"));
+	 
+	 }
+
 
 }
